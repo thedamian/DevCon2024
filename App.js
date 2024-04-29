2490faaab9f21150269fab042d24c95521e8edfd
Camila Sandoval <83136741+camilasandovals@users.noreply.github.com>
2024-04-29 10:20:51 -0400

    done

--- App.js
+++ App.js
@@ -1,20 +1,290 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useRef } from "react";
+import { Text, View, TouchableOpacity, SafeAreaView, Image, Button, Modal, StyleSheet } from "react-native";
+import { Camera, CameraType } from "expo-camera";
+import { Feather, FontAwesome6, AntDesign, MaterialIcons } from "@expo/vector-icons";
+import { uploadPicture } from "./post.js";
 
 export default function App() {
+  const [foodList, setFoodList] = useState([]);
+  const [shoppingTotal, setShoppingTotal] = useState(0);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [showCamera, setShowCamera] = useState(false);
+  const [cameraType, setCameraType] = useState(CameraType.back);
+  const cameraRef = useRef(null);
+  const [pictureUri, setPictureUri] = useState(null);
+  const [analysisResult, setAnalysisResult] = useState(null);
+  const [modalConfig, setModalConfig] = useState({
+    visible: false,
+    content: null,
+    onConfirm: () => {},
+  });
+
+  const ConfirmationModal = () => (
+    <Modal animationType="slide" transparent={true} visible={modalConfig.visible}
+      onRequestClose={() =>
+        setModalConfig((prev) => ({ ...prev, visible: false }))
+      }
+    >
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+          {modalConfig.content}
+          <View style={styles.buttonContainer}>
+            <MaterialIcons name="cancel" size={60} color="#f77070"
+              onPress={() =>
+                setModalConfig((prev) => ({ ...prev, visible: false }))
+              }
+            />
+            <AntDesign name="checkcircle" size={55} color="#8dc88d" 
+            onPress={() => {
+                modalConfig.onConfirm();
+                setModalConfig((prev) => ({ ...prev, visible: false }));
+              }}
+            />
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+
+  const showAddConfirmation = (result, uri) => {
+    setModalConfig({
+      visible: true,
+      content: (
+        <>
+          <Text style={styles.modalText}>
+            {result.food} average price is ${result.price}
+          </Text>
+          <Image source={{ uri: uri }} style={styles.previewImage} />
+        </>
+      ),
+      onConfirm: () => {
+        const newItem = { ...result, imageUri: uri };
+        const newFoodList = [...foodList, newItem];
+        setFoodList(newFoodList);
+        const additionalPrice = Number(result.price);
+        const newTotal = parseFloat(shoppingTotal) + additionalPrice;
+        setShoppingTotal(newTotal.toFixed(2));
+      }
+    });
+  };
+
+  const takePicture = async () => {
+    if (cameraRef.current) {
+      try {
+        const photo = await cameraRef.current.takePictureAsync();
+        setShowCamera(false);
+        setPictureUri(photo.uri);
+        const result = await uploadPicture(photo.uri);
+        if (result.food === "unknown" || result.price === "unknown") {
+          return;
+        }
+        setAnalysisResult(result);
+        showAddConfirmation(result, photo.uri);
+      } catch (error) {
+        console.error("Error taking or uploading picture:", error);
+      } 
+    }
+  };
+
+
+  if (showCamera) {
+    if (!permission?.granted) {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.text}>
+            We need your permission to show the camera
+          </Text>
+          <Button onPress={requestPermission} title="Grant Permission" />
+        </View>
+      );
+    }
+
+    return (
+      <Camera style={styles.camera} type={cameraType} ref={cameraRef}>
+        <View style={styles.cameraIcons}>
+          <TouchableOpacity onPress={() => setShowCamera(false)}>
+            <AntDesign name="close" size={24} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.pictureButton} onPress={takePicture}></TouchableOpacity>
+          <TouchableOpacity onPress={() => setCameraType((prev) => (prev === CameraType.back ? CameraType.front : CameraType.back))}>
+            <FontAwesome6 name="arrows-rotate" size={24} color="white" />
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+       
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    backgroundColor: "#dfe4ea",
+  },
+  text: {
+    fontSize: 23,
+    color: "#35374B",
+  },
+  cartApp: {
+    fontSize: 35,
+    alignSelf: "flex-start",
+    marginVertical: 10,
+    fontWeight: 700,
+    color: "#786DAE",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowOpacity: 0.8,
+    shadowRadius: 0.5,
+    elevation: 1,
+    margin: 20,
+  },
+  total: {
+    fontSize: 35,
+    color: "#35374B",
+    fontWeight: 300,
+    margin: 35,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowRadius: 1,
+    shadowOpacity: 0.2,
+  },
+  foodListContainer: {
+    position: "relative",
+    flex: 1,
+    width: 350,
+    backgroundColor: "white",
+    paddingVertical: 10,
+    paddingHorizontal: 35,
+    borderRadius: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    margin: 20,
+  },
+  titleList: {
+    fontSize: 21,
+    fontWeight: 500,
+    marginVertical: 20,
+    color: "lightgray",
+  },
+  resetButton: {
+    alignSelf: "flex-end",
+    backgroundColor: "#968ec0",
+    borderRadius: 15,
+    position: "absolute",
+    right: -25,
+  },
+  foodItemContainer: {
+    width: "100%",
+    padding: 10,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    color: "#35374B",
+    borderBottomColor: "lightgray",
+    borderBottomWidth: 1,
+    fontSize: 20,
+  },
+  deleteButton: {
+    padding: 10,
+  },
+  emptyText: {
+    fontSize: 18,
+    color: "gray",
+    textAlign: "center",
+    margin: 30,
+  },
+  iconButton: {
+    padding: 10,
+    borderRadius: 20,
+    width: 60,
+    height: 60,
+    backgroundColor: "#968ec0",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 30,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    width: "85%",
+  },
+  modalText: {
+    fontSize: 18,
+    color: "#35374B",
+    textAlign: "center",
+  },
+  buttonContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    width: "100%",
+    margin: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  cameraIcons: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "flex-end",
+    marginBottom: 60,
+  },
+  pictureButton: {
+    padding: 10,
+    borderRadius: 30,
+    width: 60,
+    height: 60,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  previewImage: {
+    width: 250,
+    height: 250,
+    margin: 20,
   },
 });
---
+++ README.md
@@ -0,0 +1,107 @@
+# CartApp 🛒
+
+To run the app, follow these steps:
+
+1. Go to `https://snack.expo.dev/`
+2. Add the following packages to your `package.json` file:
+
+```json
+"expo-camera": "~14.1.2",
+"expo-file-system": "~16.0.9"
+```
+
+3. Create the `post.js` file in snack and paste the code from `post.js` into the editor
+4. Paste the code from `App.js` into the editor
+5. Run the app on your device
+
+## -API Setup
+
+The CartApp utilizes FastAPI and OpenAI's GPT-4 Turbo model. 
+
+Here's how to set up the API:
+
+1. Go to `https://platform.openai.com/` and login or create an account
+2. Inside of settings add the billing information
+3. Create an API key
+4. Go to `https://replit.com/` and create a replit account
+5. Click on the `+ Create Repl` button to create a new repl
+6. Select the `FastAPI` template
+7. Install the required packages
+
+pip install the following packages:
+
+```bash
+pip install openai
+pip install python-multipart
+```
+
+### Create a FastAPI Server
+Create a FastAPI server with a POST endpoint that accepts an image file and returns the food item and its estimated price.
+
+```python
+
+from fastapi import FastAPI, File, HTTPException, UploadFile
+from openai import OpenAI
+import base64
+
+app = FastAPI()
+client = OpenAI(api_key="REPLACE WITH YOUR API KEY")
+
+@app.get("/")
+def read_root():
+    return {"Hello": "Grammerhub"}
+
+@app.post("/")
+async def get_openai_response_post(image: UploadFile = File(...)):
+  try:
+    base64_image = await convert_image_to_base64(image) # convert image to base64
+
+    # send the image to OpenAI's GPT-4 Turbo model
+    response = client.chat.completions.create(
+        model="gpt-4-turbo",
+        messages=[{
+            "role":
+            "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": system_prompt
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/png;base64,{base64_image}"
+                    },
+                },
+            ],
+        }],
+        max_tokens=15, # limits how long the response can be
+        top_p=0.1) #uses common and fitting words instead of rare or strange ones so it makes the response more predictable
+
+    # extract the food and price from the response
+    food, price = response.choices[0].message.content.split('=')
+
+    return {"food": food, "price": price}
+
+  except Exception as e:
+    print(f"Error: {e}")
+    raise HTTPException(status_code=500, detail=str(e))
+
+
+async def convert_image_to_base64(image: UploadFile):
+  """Base64 is a method used to encode data, such as images or text files, into a string of characters that can be easily transmitted over the internet or stored in a text-based format."""
+  image_content = await image.read()
+  return base64.b64encode(image_content).decode('utf-8')
+
+
+system_prompt = """
+You are an agent specialized in tagging images of food and proving its possible price.
+You will be provided with an image and your goal is to identify what food it is and it's estimated price.
+The price shouldn't be the most updated, just give an estimate from stores like Walmart, Publix, Whole Foods, etc.
+Return the food and the price in the format of a string separated with an equal sign, like this: Oldfashioned Oatmeal=3.99
+If it's not food or you can't identify the price just return 'unknown' for both price and food.
+"""
+```
+
+### Documentation:
+https://platform.openai.com/docs/guides/vision
